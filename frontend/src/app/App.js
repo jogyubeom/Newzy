d7fb3ca0baf2117e2c08b5c64a400a588345bf20
김해인 <snnsprng@naver.com>
2024-09-12 09:55:27 +0900

    feat:FSD 디렉토리 구조 설정

---
+++ frontend/src/app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>사이드바입니다.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
