6c264426e6b13883d91603bd07e2b945b818d8c3
조규범 <standardjo@naver.com>
2024-09-12 15:25:08 +0900

    개발환경 정리

---
+++ frontend/src/pages/profile/profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <div className="Profile">
+      <h2>여기는 Profile Page</h2>
+      <p>{new Date().toLocaleString()}</p>
+    </div>
+  );
+};
+
+export default Profile;
