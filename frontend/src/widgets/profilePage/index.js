e2494f998f38b38e2de706f55f5c5ce2506fd435
조규범 <standardjo@naver.com>
2024-09-23 12:49:49 +0900

    error rreal cut

--- frontend/src/widgets/profilePage/index.js
+++ frontend/src/widgets/profilePage/index.js
@@ -1,5 +1,5 @@
-export { bookMark } from "./bookMark";
+export { bookMark } from "widgets/profilePage/bookMark";
 export { words } from "./words";
 export { FollowIndex } from "./followIndex";
-export { myNewzy } from "./myNewzy";
-export { menuBar } from "./menuBar";
+export { myNewzy } from "widgets/profilePage/myNewzy";
+export { menuBar } from "widgets/profilePage/menuBar";
