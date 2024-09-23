880870967d46b4f5b35aef158a04548dbf61fa08
조규범 <standardjo@naver.com>
2024-09-23 12:43:08 +0900

    dsad

--- frontend/src/widgets/profilePage/index.js
+++ frontend/src/widgets/profilePage/index.js
@@ -1,5 +1,5 @@
 export { bookMark } from "./bookMark";
-export { menuBar } from "./menuBar";
-export { myNewzy } from "./myNewzy";
 export { words } from "./words";
 export { FollowIndex } from "./followIndex";
+export { myNewzy } from "./myNewzy";
+export { menuBar } from "./menuBar";
