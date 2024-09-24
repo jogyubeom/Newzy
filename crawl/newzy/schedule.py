c6d6a0a76d06638691d32172f1056d0cda00c3d5
강윤서 <ysys1130@gmail.com>
2024-09-24 13:14:14 +0900

    refactor: scheduler 시간 변경

--- crawl/newzy/schedule.py
+++ crawl/newzy/schedule.py
@@ -17,12 +17,12 @@ def start_scheduler():
         scheduler = BackgroundScheduler()
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
-        # # 시간에 맞춰 작업을 예약
-        # for time in times:
-        #     scheduler.add_job(schedule_batch_task, 'cron', hour=time, minute=0)
+        # 시간에 맞춰 작업을 예약
+        for time in times:
+            scheduler.add_job(schedule_batch_task, 'cron', hour=time, minute=0)
 
         # TEST
-        scheduler.add_job(schedule_batch_task, 'cron', hour=10, minute=4)
+        # scheduler.add_job(schedule_batch_task, 'cron', hour=10, minute=4)
         scheduler.start()
 
 
@@ -33,18 +33,19 @@ def schedule_batch_task():
     # end_date는 현재 시간 (배치가 실행되는 순간)
     end_date = now.replace(minute=00, second=0, microsecond=0)
 
-    # # 현재 end_date가 times 리스트에서 몇 번째 인덱스인지 찾음
-    # end_index = times.index(end_date.hour)
-    #
-    # # start_date는 end_index의 이전 값, 만약 end_index가 0이면 전날 20시로 설정
-    # if end_index == 0:
-    #     start_date = (end_date - timedelta(days=1)).replace(hour=20)
-    # else:
-    #     start_date = end_date.replace(hour=times[end_index - 1])
+    # 현재 end_date가 times 리스트에서 몇 번째 인덱스인지 찾음
+    end_index = times.index(end_date.hour)
+
+    # start_date는 end_index의 이전 값, 만약 end_index가 0이면 전날 20시로 설정
+    if end_index == 0:
+        start_date = (end_date - timedelta(days=1)).replace(hour=20)
+    else:
+        start_date = end_date.replace(hour=times[end_index - 1])
 
     # TEST
     # start_date = (end_date - timedelta(days=1)).replace(hour=16)
-    start_date = end_date.replace(hour=6)
+    # start_date = end_date.replace(hour=6)
+    
     logging.info(
         f"########### Batch task started. Start date: {start_date}, End date: {end_date} ###########")
 
