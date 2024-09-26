30b28b1f006a22f9bcd49b91695f31d0e3334f61
김해인 <snnsprng@naver.com>
2024-09-26 17:38:17 +0900

    Card Directory

---
+++ frontend/src/entities/card/cardDeck.jsx
@@ -0,0 +1,3 @@
+export const CardDeck = () => {
+  return <div>마이페이지 카드 뭉탱이</div>;
+};
---
+++ frontend/src/entities/card/cardFront.jsx
@@ -0,0 +1,8 @@
+export const CardFront = () => {
+  return (
+    <div>
+      마이페이지 카드 앞면
+      <div> 카드 획득 후 기사 옆에 앞면</div>
+    </div>
+  );
+};
