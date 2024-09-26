13c3133d4801a2c5da76ec7f6fd8c6d9f518d43f
강윤서 <ysys1130@gmail.com>
2024-09-26 23:34:11 +0900

    feat: kill_driver_processes 추가

--- crawl/newzy/tasks.py
+++ crawl/newzy/tasks.py
@@ -1,10 +1,15 @@
 import logging
+import os
 from datetime import datetime
+
+import psutil
 
 from newzy.config import create_webdriver
 from newzy.utils.economy_crawler import hankyung, mk, mt
 from newzy.utils.international_crawler import newsis, segye, sbsbiz
 from newzy.utils.society_crawler import yna, ytn, channelA
+
+logging = logging.getLogger('my_logger')
 
 
 def economy(driver, start_times, end_times, difficulty_distribution, start_date, end_date):
@@ -21,7 +26,6 @@ def economy(driver, start_times, end_times, difficulty_distribution, start_date,
        difficulty_distribution, start_date, end_date)
     mt(driver, economy_article_count_by_hour, economy_mt_news_link_list, start_times, end_times,
        difficulty_distribution, start_date, end_date)
-
 
     return
 
@@ -65,6 +69,23 @@ def international(driver, start_times, end_times, difficulty_distribution, start
     return
 
 
+def kill_driver_processes():
+    # 현재 프로세스의 PID를 구함 (run_crawl의 프로세스 ID)
+    current_pid = os.getpid()
+
+    # 현재 프로세스에서 실행 중인 모든 하위 프로세스를 검색
+    for proc in psutil.process_iter(['pid', 'name', 'ppid']):
+        try:
+            # 프로세스 이름이 'chrome' 또는 'chromedriver'인지 확인하고
+            # 부모 프로세스 ID(ppid)가 current_pid와 일치하는 경우만 종료
+            if (proc.info['ppid'] == current_pid) and (
+                    'chromedriver' in proc.info['name'] or 'chrome' in proc.info['name']):
+                proc.kill()
+                print(f"Killed process {proc.info['name']} with PID {proc.info['pid']}")
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
 def run_crawl(start_date, end_date):
     start_times = {}
     end_times = {}
@@ -74,14 +95,15 @@ def run_crawl(start_date, end_date):
 
     try:
         # 여러 메서드에서 같은 driver를 공유
-        economy(start_times, end_times, difficulty_distribution, start_date, end_date, driver)
-        society(start_times, end_times, difficulty_distribution, start_date, end_date, driver)
-        international(start_times, end_times, difficulty_distribution, start_date, end_date, driver)
+        economy(driver, start_times, end_times, difficulty_distribution, start_date, end_date)
+        society(driver, start_times, end_times, difficulty_distribution, start_date, end_date)
+        international(driver, start_times, end_times, difficulty_distribution, start_date, end_date)
     finally:
         # 모든 작업이 끝난 후에 드라이버 종료
         driver.quit()
+        kill_driver_processes()
 
-    # 각 프로세스의 시작과 종료 시간 출력 및 총 소요 시간 계산
+        # 각 프로세스의 시작과 종료 시간 출력 및 총 소요 시간 계산
     for process_name in start_times.keys():
         start_time = start_times.get(process_name)
         end_time = end_times.get(process_name, None)
