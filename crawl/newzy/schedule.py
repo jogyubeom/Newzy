e371fb10e3c15b9484f30655cd5a416cc313e408
강윤서 <ysys1130@gmail.com>
2024-09-27 10:19:40 +0900

    refactor: scheduler 주기 변경

--- crawl/newzy/schedule.py
+++ crawl/newzy/schedule.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -40,12 +40,11 @@ def schedule_batch_task():
 
     end_index = BATCH_TIMES.index(end_date.hour)
 
-    start_date = end_date.replace(hour=BATCH_TIMES[end_index - 1])
-
-    # if end_index == 0:
-    #     start_date = (end_date - timedelta(days=1)).replace(hour=20)
-    # else:
-    #     start_date = end_date.replace(hour=BATCH_TIMES[end_index - 1])
+    if end_index == 0:
+        # start_date = (end_date - timedelta(days=1)).replace(hour=20)
+        start_date = end_date.replace(hour=10)
+    else:
+        start_date = end_date.replace(hour=BATCH_TIMES[end_index - 1])
 
     logging.info(
         f"########### Batch task started. Start date: {start_date}, End date: {end_date} ###########")
