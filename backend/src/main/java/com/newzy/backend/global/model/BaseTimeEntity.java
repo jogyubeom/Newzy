3088d35c44473fd3cc38ccae4b703c6b8420b504
Young1 <dlduddnjs198@naver.com>
2024-10-01 20:28:45 +0900

    refactor: BaseResponseBody, BaseTimeEntity 정리

--- backend/src/main/java/com/newzy/backend/global/model/BaseTimeEntity.java
+++ backend/src/main/java/com/newzy/backend/global/model/BaseTimeEntity.java
@@ -2,7 +2,6 @@ package com.newzy.backend.global.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -13,18 +12,19 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
     // created_at
     @Column(name = "create_at", updatable = false)
-    private LocalDateTime createdAt ;
+    private LocalDateTime createdAt;
 
     // updated_at
     @Column(name = "update_at")
-    private LocalDateTime updatedAt ;
+    private LocalDateTime updatedAt;
 
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
 
-    @PreUpdate void onUpdate() {
+    @PreUpdate
+    void onUpdate() {
         this.updatedAt = LocalDateTime.now();
     }
 }
