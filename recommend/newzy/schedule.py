94d31fdb7fa623effde9b397c016d32ed6272bcf
강윤서 <ysys1130@gmail.com>
2024-10-01 17:31:17 +0900

    feat: scheduler 시간 변경

--- recommend/newzy/schedule.py
+++ recommend/newzy/schedule.py
@@ -1,7 +1,6 @@
 import logging
 import os
 
-from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 
@@ -19,7 +18,7 @@ def start_scheduler():
 
         time = 23  # 매일 23시마다 군집 별 추천 알고리즘 적용 -> 캐시에 저장
         job_id = f"recommend_batch_task_{time}_daily"
-        scheduler.add_job(schedule_batch_task, 'cron', hour=time, minute=00, id=job_id,
+        scheduler.add_job(schedule_batch_task, 'cron', hour=time, minute=0, id=job_id,
                           replace_existing=True)
         scheduler.start()
 
