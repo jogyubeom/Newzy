21cd38971bb2b55e2d0137999de8fa2c0946a224
Young1 <dlduddnjs198@naver.com>
2024-10-03 04:54:39 +0900

    refactor: EXPIRATION_TIME 추가

--- backend/src/main/java/com/newzy/backend/domain/user/service/AuthService.java
+++ backend/src/main/java/com/newzy/backend/domain/user/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.newzy.backend.domain.user.service;
 
 public interface AuthService {
+    final int EXPIRATION_TIME = 3600;
     String handleLoginOrSignup(String code);
 
     String generateLoginUrl();
