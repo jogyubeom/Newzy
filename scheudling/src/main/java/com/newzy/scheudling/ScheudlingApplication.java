9374a572d36cabf13a19740cfab06546e81eead2
Young1 <dlduddnjs198@naver.com>
2024-10-05 02:52:11 +0900

    add: scheduling initial commit

---
+++ scheudling/src/main/java/com/newzy/scheudling/ScheudlingApplication.java
@@ -0,0 +1,13 @@
+package com.newzy.scheudling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheudlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheudlingApplication.class, args);
+    }
+
+}
---
+++ scheudling/src/test/java/com/newzy/scheudling/ScheudlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newzy.scheudling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheudlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
