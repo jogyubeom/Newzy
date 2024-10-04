1187942b0f9cfc25bdab6e8b61de3a88d636b471
강윤서 <ysys1130@gmail.com>
2024-10-04 14:29:56 +0900

    feat: 나만의 단어장 수정

--- backend/src/main/java/com/newzy/backend/domain/dictionary/service/DictionaryService.java
+++ backend/src/main/java/com/newzy/backend/domain/dictionary/service/DictionaryService.java
@@ -1,6 +1,7 @@
 package com.newzy.backend.domain.dictionary.service;
 
 import com.newzy.backend.domain.dictionary.dto.request.SearchWordRequestDTO;
+import com.newzy.backend.domain.dictionary.dto.request.VocaListRequestDTO;
 import com.newzy.backend.domain.dictionary.dto.response.DictionaryResponseDTO;
 import com.newzy.backend.domain.dictionary.dto.response.VocaListResponseDTO;
 import com.newzy.backend.domain.dictionary.dto.response.WordCloudResponseDTO;
@@ -10,8 +11,8 @@ import java.util.List;
 public interface DictionaryService {
     List<DictionaryResponseDTO> searchByWord(String word);
 
-    void addSearchWordHistory(Long userId, Long newsId, String word);
-    List<VocaListResponseDTO> getSearchWordHistory(SearchWordRequestDTO searchWordRequestDTO);
+    void addVocaList(Long userId, VocaListRequestDTO vocaListRequestDTO);
+    List<VocaListResponseDTO> getVocaList(SearchWordRequestDTO searchWordRequestDTO);
     void deleteSearchWordHistory(Long userId, String word);
     List<WordCloudResponseDTO> getWordCloudHistory();
 
