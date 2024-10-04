9df99262fc5017f1e4b4e060e8c387db0a5e6e79
이영원 <dlduddnjs198@naver.com>
2024-10-05 03:57:07 +0900

    refactor: Update test

--- scheduling/src/test/java/com/newzy/scheudling/ScheudlingApplicationTests.java
+++ scheduling/src/test/java/com/newzy/scheudling/ScheudlingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ScheudlingApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    // @Test
+    // void contextLoads() {
+    // }
 
 }
