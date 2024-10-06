274b5f2312e106659c12c0dc436cce409f81c99a
Young1 <dlduddnjs198@naver.com>
2024-10-07 03:04:12 +0900

    refactor: news 조회수 redis 저장 방식으로 변경

--- backend/src/main/java/com/newzy/backend/domain/news/service/NewsService.java
+++ backend/src/main/java/com/newzy/backend/domain/news/service/NewsService.java
@@ -3,10 +3,9 @@ package com.newzy.backend.domain.news.service;
 import com.newzy.backend.domain.news.dto.request.NewsCardRequestDTO;
 import com.newzy.backend.domain.news.dto.request.NewsListGetRequestDTO;
 import com.newzy.backend.domain.news.dto.response.*;
-import com.newzy.backend.domain.news.dto.request.NewsListGetRequestDTO;
 import com.newzy.backend.domain.news.dto.response.NewsDailyGetResponseDTO;
-import com.newzy.backend.domain.news.dto.response.NewsDetailGetResponseDto;
-import com.newzy.backend.domain.news.dto.response.NewsListGetResponseDto;
+import com.newzy.backend.domain.news.dto.response.NewsDetailGetResponseDTO;
+import com.newzy.backend.domain.news.dto.response.NewsListGetResponseDTO;
 import com.newzy.backend.domain.news.dto.response.NewsRecommendGetResponseDTO;
 
 import java.util.List;
@@ -14,7 +13,7 @@ import java.util.Map;
 
 public interface NewsService {
     Map<String, Object> getNewsList(NewsListGetRequestDTO newsListGetRequestDTO);
-    NewsDetailGetResponseDto getNewsDetail(Long NewsId);
+    NewsDetailGetResponseDTO getNewsDetail(Long NewsId);
 
     void bookmark(Long userId, Long NewsId);
     void deleteBookmark(Long userId, Long newsId);
@@ -23,11 +22,11 @@ public interface NewsService {
     void deleteLike(Long userId, Long NewsId);
 
     Map<String, Object> getCardList(Long userId, int page);
-    List<NewsListGetResponseDto> getHotNewsList();
+    List<NewsListGetResponseDTO> getHotNewsList();
     List<NewsRecommendGetResponseDTO> getRecommendedNewsList(Long userId);
 
     NewsDailyGetResponseDTO getDailyContent(Long userId);
 
     void collectNewsCard(Long userId, NewsCardRequestDTO newsCardRequestDTO);
-    NewsCardListGetResponseDto getCardInfo(Long userId, Long cardId);
+    NewsCardListGetResponseDTO getCardInfo(Long userId, Long cardId);
 }
