71ae885f09fa87df7ec6e00873014cd25a2c3162
dayullee <dy007lukyday@gmail.com>
2024-10-07 09:36:18 +0900

    fix: 뉴지 목록 반환 오류 수정

--- backend/src/main/java/com/newzy/backend/domain/newzy/repository/NewzyLikeRepositorySupport.java
+++ backend/src/main/java/com/newzy/backend/domain/newzy/repository/NewzyLikeRepositorySupport.java
@@ -46,7 +46,7 @@ public class NewzyLikeRepositorySupport  extends QuerydslRepositorySupport {
         int totalPage = (int) ((totalCount + size - 1) / size);
 
         List<NewzyListGetResponseDTO> newzyList = queryFactory
-                .select(Projections.constructor(NewzyListGetResponseDTO.class,
+                .select(Projections.fields(NewzyListGetResponseDTO.class,
                         qNewzy.newzyId,
                         qNewzy.user.nickname,
                         qNewzy.user.userId,
