54b71a1299d6d17217d0569b92ae02b05838f879
강윤서 <ysys1130@gmail.com>
2024-10-07 15:31:58 +0900

    refactor: chromedriver 독립 생성

--- crawl/newzy/tasks.py
+++ crawl/newzy/tasks.py
@@ -12,7 +12,7 @@ from newzy.utils.society_crawler import yna, ytn, channelA
 logging = logging.getLogger('my_logger')
 
 
-def economy(driver, start_times, end_times, difficulty_distribution, start_date, end_date):
+def economy(start_times, end_times, difficulty_distribution, start_date, end_date):
     logging.info(f"경제 카테고리 기사 크롤링 시작 - start_date: {start_date}, end_date: {end_date}")
 
     economy_article_count_by_hour = {f'{hour:02d}': 0 for hour in range(24)}
@@ -20,17 +20,21 @@ def economy(driver, start_times, end_times, difficulty_distribution, start_date,
     economy_mk_news_link_list = []  # 2. 매일 경제
     economy_mt_news_link_list = []  # 3. 머니 투데이
 
-    hankyung(driver, economy_article_count_by_hour, economy_hankyung_news_link_list, start_times,
-             end_times, difficulty_distribution, start_date, end_date)
-    mk(driver, economy_article_count_by_hour, economy_mk_news_link_list, start_times, end_times,
-       difficulty_distribution, start_date, end_date)
-    mt(driver, economy_article_count_by_hour, economy_mt_news_link_list, start_times, end_times,
-       difficulty_distribution, start_date, end_date)
+    driver = create_webdriver()
+    try:
+        hankyung(driver, economy_article_count_by_hour, economy_hankyung_news_link_list, start_times,
+                 end_times, difficulty_distribution, start_date, end_date)
+        mk(driver, economy_article_count_by_hour, economy_mk_news_link_list, start_times, end_times,
+           difficulty_distribution, start_date, end_date)
+        mt(driver, economy_article_count_by_hour, economy_mt_news_link_list, start_times, end_times,
+           difficulty_distribution, start_date, end_date)
+    finally:
+        driver.close()
+        driver.quit()
+        kill_driver_processes()
 
-    return
 
-
-def society(driver, start_times, end_times, difficulty_distribution, start_date, end_date):
+def society(start_times, end_times, difficulty_distribution, start_date, end_date):
     logging.info(f"사회 카테고리 기사 크롤링 시작 - start_date: {start_date}, end_date: {end_date}")
 
     society_article_count_by_hour = {f'{hour:02d}': 0 for hour in range(24)}
@@ -38,17 +42,21 @@ def society(driver, start_times, end_times, difficulty_distribution, start_date,
     society_ytn_news_link_list = []  # 2. YTN
     society_channelA_news_link_list = []  # 3. 채널 A
 
-    yna(driver, society_article_count_by_hour, society_yna_news_link_list, start_times, end_times,
-        difficulty_distribution, start_date, end_date)
-    # ytn(driver, society_article_count_by_hour, society_ytn_news_link_list, start_times, end_times,
-    #     difficulty_distribution, start_date, end_date)
-    channelA(driver, society_article_count_by_hour, society_channelA_news_link_list, start_times,
-             end_times, difficulty_distribution, start_date, end_date)
+    driver = create_webdriver()
+    try:
+        yna(driver, society_article_count_by_hour, society_yna_news_link_list, start_times, end_times,
+            difficulty_distribution, start_date, end_date)
+        # ytn(driver, society_article_count_by_hour, society_ytn_news_link_list, start_times, end_times,
+        #     difficulty_distribution, start_date, end_date)
+        channelA(driver, society_article_count_by_hour, society_channelA_news_link_list, start_times,
+                 end_times, difficulty_distribution, start_date, end_date)
+    finally:
+        driver.close()
+        driver.quit()
+        kill_driver_processes()
 
-    return
 
-
-def international(driver, start_times, end_times, difficulty_distribution, start_date, end_date):
+def international(start_times, end_times, difficulty_distribution, start_date, end_date):
     logging.info(f"세계 카테고리 기사 크롤링 시작 - start_date: {start_date}, end_date: {end_date}")
 
     international_article_count_by_hour = {f'{hour:02d}': 0 for hour in range(24)}
@@ -56,17 +64,21 @@ def international(driver, start_times, end_times, difficulty_distribution, start
     international_segye_news_link_list = []  # 2. 세계일보
     international_sbs_biz_news_link_list = []  # 3. SBS Biz
 
-    newsis(driver, international_article_count_by_hour, international_newsis_news_link_list,
-           start_times, end_times,
-           difficulty_distribution, start_date, end_date)
-    segye(driver, international_article_count_by_hour, international_segye_news_link_list,
-          start_times, end_times,
-          difficulty_distribution, start_date, end_date)
-    sbsbiz(driver, international_article_count_by_hour, international_sbs_biz_news_link_list,
-           start_times, end_times,
-           difficulty_distribution, start_date, end_date)
-
-    return
+    driver = create_webdriver()
+    try:
+        newsis(driver, international_article_count_by_hour, international_newsis_news_link_list,
+               start_times, end_times,
+               difficulty_distribution, start_date, end_date)
+        segye(driver, international_article_count_by_hour, international_segye_news_link_list,
+              start_times, end_times,
+              difficulty_distribution, start_date, end_date)
+        sbsbiz(driver, international_article_count_by_hour, international_sbs_biz_news_link_list,
+               start_times, end_times,
+               difficulty_distribution, start_date, end_date)
+    finally:
+        driver.close()
+        driver.quit()
+        kill_driver_processes()
 
 
 def kill_driver_processes():
@@ -101,35 +113,28 @@ def run_crawl(start_date, end_date):
     end_times = {}
     difficulty_distribution = {}
 
-    driver = create_webdriver()  # 드라이버를 한 번만 생성
-
     try:
-        # 여러 메서드에서 같은 driver를 공유
-        economy(driver, start_times, end_times, difficulty_distribution, start_date, end_date)
-        society(driver, start_times, end_times, difficulty_distribution, start_date, end_date)
-        international(driver, start_times, end_times, difficulty_distribution, start_date, end_date)
+        # 각 크롤링 작업에서 독립된 driver를 생성 및 사용
+        economy(start_times, end_times, difficulty_distribution, start_date, end_date)
+        society(start_times, end_times, difficulty_distribution, start_date, end_date)
+        international(start_times, end_times, difficulty_distribution, start_date, end_date)
     finally:
-        # 모든 작업이 끝난 후에 드라이버 종료
-        driver.close()
-        driver.quit()
-        kill_driver_processes()
-
         # 각 프로세스의 시작과 종료 시간 출력 및 총 소요 시간 계산
-    for process_name in start_times.keys():
-        start_time = start_times.get(process_name)
-        end_time = end_times.get(process_name, None)
+        for process_name in start_times.keys():
+            start_time = start_times.get(process_name)
+            end_time = end_times.get(process_name, None)
 
-        if end_time:
-            elapsed_time = end_time - start_time
-            start_time_fmt = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
-            end_time_fmt = datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
-            elapsed_time_fmt = round(elapsed_time, 2)
+            if end_time:
+                elapsed_time = end_time - start_time
+                start_time_fmt = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
+                end_time_fmt = datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
+                elapsed_time_fmt = round(elapsed_time, 2)
 
-            # 결과 출력
-            logging.info(f"{process_name} 시작 시간: {start_time_fmt}")
-            logging.info(f"{process_name} 종료 시간: {end_time_fmt}")
-            logging.info(f"{process_name} 총 소요 시간: {elapsed_time_fmt} 초")
-            logging.info("-" * 40)  # 구분선 추가
-        else:
-            logging.warning(f"{process_name} 종료 시간을 기록하지 못했습니다.")
-            logging.info("-" * 40)  # 구분선 추가
+                # 결과 출력
+                logging.info(f"{process_name} 시작 시간: {start_time_fmt}")
+                logging.info(f"{process_name} 종료 시간: {end_time_fmt}")
+                logging.info(f"{process_name} 총 소요 시간: {elapsed_time_fmt} 초")
+                logging.info("-" * 40)  # 구분선 추가
+            else:
+                logging.warning(f"{process_name} 종료 시간을 기록하지 못했습니다.")
+                logging.info("-" * 40)  # 구분선 추가
