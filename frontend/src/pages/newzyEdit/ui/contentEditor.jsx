62238077ae56dee59e7327f723a9d52a90e6f2ab
김예영 <songgang4579@naver.com>
2024-10-08 10:43:28 +0900

    fix: 글 작성 시 이미지 삽입 로직 추가

--- frontend/src/pages/newzyEdit/ui/contentEditor.jsx
+++ frontend/src/pages/newzyEdit/ui/contentEditor.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useMemo, useRef } from 'react';
 import ReactQuill from 'react-quill-new';
+import axios from "axios";
+import ImageResize from 'quill-image-resize';
 import 'react-quill-new/dist/quill.snow.css';
 
+Quill.register('modules/ImageResize', ImageResize);
+
 const ContentEditor = ({ content, setContent }) => {
-  const modules = {
+  const quillRef = useRef(null); // ref 생성
+
+  const imageHandler = () => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.addEventListener('change', async () => {
+      const file = input.files[0];
+      const formData = new FormData();
+      formData.append("img", file);
+
+      try {
+        const res = await axios.get("https://j11b305.p.ssafy.io/api/newzy/upload-image", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        const imgUrl = res.data.imgUrl;
+        const editor = quillRef.current.getEditor();
+        const range = editor.getSelection();
+        editor.insertEmbed(range.index, 'image', imgUrl);
+        editor.setSelection(range.index + 1);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
+  const modules = useMemo(() => ({
     toolbar: {
       container: [
         [{ header: [1, 2, 3, 4, 5, false] }],
@@ -11,11 +45,19 @@ const ContentEditor = ({ content, setContent }) => {
         [{ align: ['justify', 'center', 'right'] }],
         ['image'],
       ],
+      handlers: { image: imageHandler },
+      clipboard: {
+        matchVisual: false,
+      },
+      ImageResize: {
+        parchment: Quill.import('parchment')
+      },
     },
-  };
+  }), []);
 
   return (
     <ReactQuill
+      ref={quillRef} // ref 연결
       style={{ width: '100%', height: '60%' }}
       modules={modules}
       value={content}
