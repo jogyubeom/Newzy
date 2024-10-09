f5748a3da0f71a843b0f19b8adeb6db355cd768a
김예영 <songgang4579@naver.com>
2024-10-09 17:04:33 +0900

    fix: css 마이너 수정

--- frontend/src/pages/newzyEdit/ui/contentEditor.jsx
+++ frontend/src/pages/newzyEdit/ui/contentEditor.jsx
@@ -13,44 +13,32 @@ const ContentEditor = ({ content, setContent }) => {
 
   // image를 서버로 전달하는 과정
   const imageHandler = () => {
-    // input type=file DOM을 만든다.
     const input = document.createElement("input");
     input.setAttribute("type", "file");
     input.setAttribute("accept", "image/*");
-    input.click(); // toolbar 이미지를 누르게 되면 이 부분이 실행된다.
-    
-    // 이미지를 선택하게 될 시
+    input.click();
+
     input.onchange = async () => {
-      // 이미지 선택에 따른 조건을 다시 한번 하게 된다.
       const file = input.files ? input.files[0] : null;
-      if (!file) return; // 선택을 안하면 취소버튼처럼 수행하게 된다.
+      if (!file) return;
 
-      // 서버에서 FormData 형식으로 받기 때문에 이에 맞는 데이터 형식으로 만들어준다.
       const formData = new FormData();
-      formData.append("image", file); // 서버의 key 값에 맞게 변경해야 합니다.
+      formData.append("image", file);
 
-      // 에디터 정보를 가져온다.
       let quillObj = quillRef.current?.getEditor();
-      if (!quillObj) return; // 에디터가 없는 경우 함수 종료
+      if (!quillObj) return;
 
-      // 에디터 커서 위치를 가져온다.
       const range = quillObj.getSelection();
-      if (!range) return; // 커서가 없다면 종료
+      if (!range) return;
 
       try {
-        // 서버에 데이터를 보내고 URL을 imgUrl로 받는다.
         const res = await baseAxios().post("/newzy/upload-image", formData, {
           headers: {
             "Content-Type": "multipart/form-data",
           },
         });
 
-        const imgUrl = res.data.url; // 서버로부터 받은 이미지 URL
-
-        console.log(res.data);
-        console.log(imgUrl);
-
-        // 에디터의 커서 위치에 이미지 요소를 삽입한다.
+        const imgUrl = res.data.url;
         quillObj.insertEmbed(range.index, "image", `${imgUrl}`);
       } catch (error) {
         console.error('Error uploading image:', error);
@@ -64,9 +52,9 @@ const ContentEditor = ({ content, setContent }) => {
         [{ header: [1, 2, 3, 4, 5, false] }],
         ['bold', 'italic', 'underline'],
         [{ align: ['justify', 'center', 'right'] }],
-        ['image'], // 이미지 삽입 버튼
+        ['image'],
       ],
-      handlers: { image: imageHandler }, // 이미지 핸들러 등록
+      handlers: { image: imageHandler },
     },
     clipboard: {
       matchVisual: false,
@@ -77,10 +65,10 @@ const ContentEditor = ({ content, setContent }) => {
   }), []);
 
   return (
-    <div style={{ width: '100%', maxWidth: '800px', overflowY: 'auto', minHeight: '400px', maxHeight: '400px' }}>
+    <div style={{ width: '100%', maxWidth: '800px', overflowY: 'auto' }}>
       <ReactQuill
-        ref={quillRef} // ref 연결
-        style={{ width: '100%', height: '100%' }} // width와 height를 100%로 설정
+        ref={quillRef}
+        style={{ minHeight: '400px', maxHeight: '400px', overflowY: 'auto' }} // 최소 및 최대 높이 설정
         modules={modules}
         value={content}
         onChange={setContent}
