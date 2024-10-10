d2e78ed526e222e61bc176b4ad39759737bba077
김해인 <snnsprng@naver.com>
2024-10-10 17:34:04 +0900

    todayNews

--- frontend/src/shared/utils/baseAxios.js
+++ frontend/src/shared/utils/baseAxios.js
@@ -16,13 +16,6 @@ function baseAxios() {
   // 요청 인터셉터 설정
   instance.interceptors.request.use(
     (config) => {
-      // // 로컬에서 기능 테스트할 때 사용하는 수동 토큰 설정 코드
-      // config.headers.Authorization =
-      // 해인 카카오쿠키(해이니)
-      // "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMDI3IiwiZXhwIjoxNzI4NDM3OTI3fQ.mCrMd5kZC4XxIexFms8cuhRc-2cHtW77SINodgrc-W0";
-      //해인 구글 쿠키(하하호호)
-      // "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMDMxIiwiZXhwIjoxNzI4NTE0OTYwfQ.TOD064SfI-SS1L5eB5CvLkXDHcVkBoKcpJp-Azcavf0";
-
       // 배포 할 때 코드
       const token = useAuthStore.getState().token; // zustand 스토어에서 토큰 가져오기
       if (token) {
